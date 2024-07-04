d304acc2fbcb5e4cce1db1bdd7e20a550e7d46b7
eason <wangyisheng2014@hotmail.com>
2024-07-04 22:44:45 +0800

    Update run.sh
    使用conda自动新建虚拟环境

--- run.sh
+++ run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-VENV_DIR="venv"
+ENV_NAME="llm_apitest"
 REQUIREMENTS_FILE="requirements.txt"
 PYTHON_SCRIPT="0_Playground.py"
 
-if [ ! -d "${VENV_DIR}" ]; then
-    python3 -m venv "${VENV_DIR}"
+# 检查conda环境是否已经存在
+if conda info --envs | grep "${ENV_NAME}" > /dev/null; then
+    echo "Activating environment ${ENV_NAME}"
+    conda activate "${ENV_NAME}"
+else
+    echo "Creating environment ${ENV_NAME}"
+    conda create --name "${ENV_NAME}" python=3.10 -y
+    conda activate "${ENV_NAME}"
 
     if [ -f "${REQUIREMENTS_FILE}" ]; then
-        ${VENV_DIR}/bin/pip install -r "${REQUIREMENTS_FILE}"
+        pip install -r "${REQUIREMENTS_FILE}"
     fi
 fi
 
-sudo "${VENV_DIR}/bin/streamlit run" "${PYTHON_SCRIPT}" "$@"
+# 运行Python脚本
+streamlit run "${PYTHON_SCRIPT}" "$@"
